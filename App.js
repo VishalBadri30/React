df5b099431a9563a57d54a1eb4cf3e58e4e032b2
VishalBadri30 <vishalkalakuntla30@gmail.com>
2023-12-14 13:28:40 +0530

    react-ep01

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am a h1"),
+    React.createElement("h2", {}, "I am a h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1"),
+    React.createElement("h2", {}, "I am a h2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
